91864817668eff2f00302131d9bf98aaa1fbaf2d
unknown <sergeyloiko71@gmail.com>
2023-09-14 08:31:38 +1100

    first init

---
+++ app/src/main/java/ru/schematics/schedule/ui/gallery/SettingFragment.java
@@ -0,0 +1,35 @@
+package ru.schematics.schedule.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import ru.schematics.schedule.databinding.FragmentSettingBinding;
+
+public class SettingFragment extends Fragment {
+
+private FragmentSettingBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+
+    binding = FragmentSettingBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+
+        //final TextView textView = binding.textSettings;
+        //galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
